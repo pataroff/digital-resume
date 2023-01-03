b72aea1a25b91e778ca23d1a0f1540d106ebbd89
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-01-03 22:11:26 +0200

    Updated sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,35 @@
-var date = new Date();
-var hour = date.getHours();
-var root = document.querySelector(':root');
+var date = new Date()
+var hour = date.getHours()
+var root = document.querySelector(':root')
 
 function dark() {
-    root.style.setProperty('--mainTextColor', 'var(--mainTextColor-dark)');
-    root.style.setProperty('--secondaryTextColor', 'var(--secondaryTextColor-dark)');
-    root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-dark)');
-    root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-dark)');
-    root.style.setProperty('--mainBGColor', 'var(--mainBgColor-dark)');
+  root.style.setProperty('--mainTextColor', 'var(--mainTextColor-dark)')
+  root.style.setProperty(
+    '--secondaryTextColor',
+    'var(--secondaryTextColor-dark)'
+  )
+  root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-dark)')
+  root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-dark)')
+  root.style.setProperty('--mainBGColor', 'var(--mainBgColor-dark)')
 }
 
 function light() {
-    root.style.setProperty('--mainTextColor', 'var(--mainTextColor-light)');
-    root.style.setProperty('--secondaryTextColor', 'var(--secondaryTextColor-light)');
-    root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-light)');
-    root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-light)');
-    root.style.setProperty('--mainBGColor', 'var(--mainBgColor-light)');
+  root.style.setProperty('--mainTextColor', 'var(--mainTextColor-light)')
+  root.style.setProperty(
+    '--secondaryTextColor',
+    'var(--secondaryTextColor-light)'
+  )
+  root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-light)')
+  root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-light)')
+  root.style.setProperty('--mainBGColor', 'var(--mainBgColor-light)')
 }
 
 function checkHour() {
-    if(hour >= 7 && hour <= 17) {
-        light();
-    } else if(hour >= 18 && hour >=6) {
-        dark();
-    }
+  if (hour >= 7 && hour <= 17) {
+    light()
+  } else if (hour >= 18 && hour >= 6) {
+    dark()
+  }
 }
 
-checkHour();
-
-
+checkHour()
